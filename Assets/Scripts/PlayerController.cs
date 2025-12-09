26120f766b78d1b15600b3a69765dbe2af9e233c
NotYourAvSalarian <notableai@gmail.com>
2025-12-09 11:33:18 -0500

    set up basic player control + obstacle consumption + prefabs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector3 m_target_pos;
+    float m_speed = 5f;
+    float m_tornado_val = 1f;
+    Vector3 m_scale_vector;
+    void Start()
+    {
+        m_scale_vector = transform.localScale;
+    }
+
+    // temp name since I just wanted to have the functionality implemented, probably better to decouple from attack and
+    // have the name be more normal later
+    public void OnAttack()
+    {
+        m_target_pos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        m_target_pos.z = transform.position.z;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, m_target_pos, m_speed * Time.deltaTime);
+        transform.localScale = m_scale_vector * m_tornado_val;
+    }
+
+    public void increaseTornadoValue(float val)
+    {
+        m_tornado_val += val;
+    }
+}

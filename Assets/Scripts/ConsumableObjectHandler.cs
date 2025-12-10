4b8c67428d50faa9f99bf552ecf428130e50e30e
NotYourAvSalarian <notableai@gmail.com>
2025-12-10 18:05:38 -0500

    Prefab value documentation update

--- Assets/Scripts/ConsumableObjectHandler.cs
+++ Assets/Scripts/ConsumableObjectHandler.cs
@@ -4,7 +4,7 @@ public class ConsumableObjectHandler : MonoBehaviour
 {
     float m_val = 1f;
     PlayerController m_player_controller;
-    // 1 = obstacle1, 2 = tree
+    // 1 = small_bush, 2 = big_bush, 3 = tree, 4 = house
     [SerializeField] int m_obstacle_type;
     void Start()
     {
